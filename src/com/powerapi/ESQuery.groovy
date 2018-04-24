08080b9b86f1f7123ba38e1809fa89847b138eec
Deblock Adrien <adrien.deblock@davidson.fr>
2018-04-24 16:13:05 +0200

    Add: choose function between POST or PUT

--- src/com/powerapi/ESQuery.groovy
+++ src/com/powerapi/ESQuery.groovy
@@ -74,7 +74,7 @@ def sendCSV2ES(String path_index, String method, String CSV) {
     println(http.inputStream) // read server response from it
 }
 
-sendCSV2ES("http://elasticsearch.app.projet-davidson.fr/powerapi/power/testing","PUT", "muid=test;timestamp=1524489876920;targets=10991;devices=cpu;power=4900.0 mW" +
+sendCSV2ES("http://elasticsearch.app.projet-davidson.fr/powerapi/power","POST", "muid=test;timestamp=1524489876920;targets=10991;devices=cpu;power=4900.0 mW" +
         "muid=test;timestamp=1524489876920;targets=10991;devices=cpu;power=4900.0 mWmuid=72e9d91f-0b77-4d48-a75c-beeef833a663;timestamp=1524489876920;targets=10991;devices=cpu;power=4900.0 mW")
 /**
  * Send JSON format to ES
