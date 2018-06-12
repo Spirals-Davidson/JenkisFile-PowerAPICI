07c3fac18eaffc5dd0ac5d0d56b00bac28f18834
Deblock Adrien <adrien.deblock@davidson.fr>
2018-06-12 14:08:54 +0200

    test

--- src/com/powerapi/Constants.groovy
+++ src/com/powerapi/Constants.groovy
@@ -1,7 +1,7 @@
 package com.powerapi
 
 class Constants {
-    static final String ACTUAL_INDEX = "powerapici"
+    static final String ACTUAL_INDEX = "powerapici_test"
     static final int NB_PAQUET = 600
     static final ELASTIC_BULK_PATH = "http://elasticsearch.app.projet-davidson.fr/_bulk"
     static final int FREQUENCY = 50
